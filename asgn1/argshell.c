9b5bc2699bba822262f857363f4844e0aa3cb199
Charlie Root <movenden@ucsc.edu>
2019-01-14 10:52:49 -0800

    simple > works

--- asgn1/argshell.c
+++ asgn1/argshell.c
@@ -1,12 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <string.h>
 
 extern char ** get_args();
 
+char** subarray(char** args, int start, int end) {
+  for (int i = 0; args[i] != NULL; i++) {
+            printf ("Argument %d: %s\n", i, args[i]);
+  }
+  printf("subarray w indicies %d, to %d\n",start,end);
+  char** sub = (char**) calloc(end-start, sizeof(char*));
+  for (int i = start; i < end; i++) {
+    sub[i - start] = (char*) calloc(strlen(args[i]) + 1, sizeof(char));
+    strcpy(sub[i - start], args[i]);
+  }
+  for (int i = 0; sub[i]; i++){
+    printf("%s\n", sub[i]);
+  }
+  return sub;
+}
+
+void execute(char* command, char* args[]) {
+  printf("executing command %s with arguments: \n", command);
+  for (int i = 0; args[i]; i++) {
+    printf("%s ",args[i]);
+  }
+  printf("\n");
+  int pid = fork();
+  if (pid) { //pid != 0                                                 
+    waitpid(pid,NULL, 0);
+  } else {
+    int s = execvp(command, args);
+    perror("Something has gone wrong!: \n");
+  }
+}
 int
 main()
 {
@@ -25,16 +57,34 @@ main()
 	    printf ("Exiting...\n");
 	    break;
 	} else {
-	  printf("doing the process thing\n");
-	  int pid = fork();
-	  if (pid) { //pid != 0
-	    printf("am parent\n");
-	    waitpid(pid,NULL, 0);	   
-	  } else {
-	    printf("am child\n");
-	    int s = execvp(args[0], &args[1]);
-	    printf("status : %d\n", s);
+	  char** newArgs = args;
+	  for (i = 0; args[i]; i++) {
+	    printf("checking %s\n", args[i]);	   
+	    if (!strcmp(args[i], "<")) {
+	      newArgs = subarray(args, 0, i);
+	      printf("Here's the subarray:\n");
+	      for (int a = 0; a < i; a++) {
+		printf("%s\n", newArgs[a]);
+	      }
+	      printf("-----------------\n");
+	      printf("Opening %s\n", args[i+1]);
+	      int fin = open(args[i+1],O_RDONLY);
+	      if (fin < 0) {
+		printf("there was a problem opening file '%s'\n", args[i+1]);
+	      }
+	      dup2(fin, STDIN_FILENO);
+	      close(fin);
+	      break;
+	    } else if (!strcmp(args[i], ">")) {
+	      newArgs = subarray(args, 0, i);
+	      int fout = open(args[i+1], O_WRONLY);
+	      dup2(fout, STDOUT_FILENO);
+	      close(fout);
+	      break;
+	    }	   
 	  }
+	  printf("let's execute\n");
+	  execute(args[0], newArgs);
 	}
     }
 }
