3edc45831ce41b49985231491198193cee26ab4d
Charlie Root <movenden@ucsc.edu>
2019-01-31 10:41:41 -0800

    fixed now

--- asgn2/copyFiles.sh
+++ asgn2/copyFiles.sh
@@ -1,5 +1,5 @@
-cp /kern/kern_switch.c  /usr/src/sys/kern/kern_switch.c ;
-cp /kern/sched_ule.c    /usr/src/sys/kern/sched_ule.c ;
-cp /sys/runq.h          /usr/src/sys/sys/runq.h ;
+cp ./kern/kern_switch.c  /usr/src/sys/kern/kern_switch.c ;
+cp ./kern/sched_ule.c    /usr/src/sys/kern/sched_ule.c ;
+cp ./sys/runq.h          /usr/src/sys/sys/runq.h ;
 
 
