151e827dc6562458e9e1bad12af823455bac85cd
Charlie Root <movenden@ucsc.edu>
2019-01-31 10:40:54 -0800

    fixed absolute paths

--- asgn2/copyFiles.sh
+++ asgn2/copyFiles.sh
@@ -1,5 +1,5 @@
-cp /usr/home/movenden/asgn2/kern/kern_switch.c  /usr/src/sys/kern/kern_switch.c ;
-cp /usr/home/movenden/asgn2/kern/sched_ule.c    /usr/src/sys/kern/sched_ule.c ;
-cp /usr/home/movenden/asgn2/sys/runq.h          /usr/src/sys/sys/runq.h ;
+cp /kern/kern_switch.c  /usr/src/sys/kern/kern_switch.c ;
+cp /kern/sched_ule.c    /usr/src/sys/kern/sched_ule.c ;
+cp /sys/runq.h          /usr/src/sys/sys/runq.h ;
 
 
