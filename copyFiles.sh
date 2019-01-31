fdf388baee5887e1a2950eea296d4670d7828797
Charlie Root <movenden@ucsc.edu>
2019-01-31 10:35:10 -0800

    script to copy files from repo to their location in sys

---
+++ copyFiles.sh
@@ -0,0 +1,5 @@
+cp /usr/home/movenden/asgn2/kern/kern_switch.c  /usr/src/sys/kern/kern_switch.c ;
+cp /usr/home/movenden/asgn2/kern/sched_ule.c    /usr/src/sys/kern/sched_ule.c ;
+cp /usr/home/movenden/asgn2/sys/runq.h          /usr/src/sys/sys/runq.h ;
+
+
