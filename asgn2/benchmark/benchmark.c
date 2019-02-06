eed68c0494ff189c0549e445e74f85232d90f36f
Ryan Blelloch <rblelloc@ucsc.edu>
2019-02-06 12:03:45 -0800

    more precision for time for benchmark

--- asgn2/benchmark/benchmark.c
+++ asgn2/benchmark/benchmark.c
@@ -14,19 +14,21 @@
 #include <limits.h>
 #include <fcntl.h>
 #include <time.h>
+#include <math.h>
 #include <sys/time.h>
 
 #include <sys/resource.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
-#define NUM_PROCS 20
+#define NUM_PROCS 5
 
 //Result structure for a single process
 struct result {
     int proc;
     int pri;
-    unsigned long time;
+    unsigned long time_s;
+    unsigned long time_ms;
     double per_exp;
     double per_act;
 };
@@ -36,12 +38,18 @@ int compare(const void *s1, const void *s2) {
 
     struct result *r1 = (struct result *)s1;
     struct result *r2 = (struct result *)s2;
-    if(r1->time < r2->time) {
+    if(r1->time_s < r2->time_s) {
         return 1;
-    } else if (r2->time < r1->time) {
+    } else if (r2->time_s < r1->time_s) {
         return -1;
     } else {
-        return 0;
+        if(r1->time_ms < r2->time_ms) {
+            return 1;
+        } else if (r2->time_ms < r1->time_ms) {
+            return -1;
+        } else {
+            return 0;
+        }
     }
 }
 
@@ -54,7 +62,7 @@ void print_results(struct result all[], int size) {
     printf("---pro---pri---tim---\n");
     for(iter = 0; iter < size; ++iter) {
       if(iter >=0) {
-            printf("%6d%6d %5lu\n", all[iter].proc, all[iter].pri, all[iter].time);
+            printf("%6d%6d %5lu.%lu\n", all[iter].proc, all[iter].pri, all[iter].time_s, all[iter].time_ms);
         }
     }
     printf("--------------------------------\n");
@@ -95,7 +103,7 @@ int main(int argc, char* argv[]) {
     char filename2[100];
 
     char result_msg[400];
-    unsigned long time_delta;
+    unsigned long time_delta_ms, time_delta_s;
 
     //default test length is 2
     int tst_length = 2;
@@ -114,7 +122,7 @@ int main(int argc, char* argv[]) {
     }
 
     //get number of procs
-    proc_num = NUM_PROCS;
+    proc_num = 20;
 
     //fork and wait for child tests to build, run, and finish
     printf("Creating processes: ");
@@ -128,7 +136,7 @@ int main(int argc, char* argv[]) {
             if(pid == 0) {
                 printf("%d, ", getpid());
                 fflush(stdout);
-                proc_num--;
+                proc_num -= 5;
             } else {
                 //get process ID for printing
                 my_pid = getpid();
@@ -142,11 +150,16 @@ int main(int argc, char* argv[]) {
                 //stop timer
                 gettimeofday(&stop, NULL);
                 //get time of test in seconds
-                time_delta =  stop.tv_sec - start.tv_sec;
+                time_delta_s = stop.tv_sec - start.tv_sec;
+                if(stop.tv_usec < start.tv_usec) {
+                    stop.tv_usec += 1e6;
+                    time_delta_s--;
+                }
+                time_delta_ms =  round((stop.tv_usec - start.tv_usec) / 1.0e3);
                 //prepare result message
-                sprintf(result_msg, "%d %d %lu\n", my_pid, pri, time_delta);
+                sprintf(result_msg, "%d %d %lu %lu\n", my_pid, pri, time_delta_s, time_delta_ms);
                 //prepare temp result file
-                sprintf(filename2, "%s%d", filename, proc_num);
+                sprintf(filename2, "%s%d", filename, proc_num/5);
                 //open temp result file to print results
                 test_results = fopen(filename2, "w");
                 if(!test_results) {
@@ -160,7 +173,7 @@ int main(int argc, char* argv[]) {
                 exit(0);
                 //printf("waiting for pid: %d\n", pid);
             }
-        } while(proc_num > 0);
+        } while(proc_num >= 0);
         printf("\n");
         exit(0);
 
@@ -175,30 +188,16 @@ int main(int argc, char* argv[]) {
     struct result all_res[NUM_PROCS];
 
     //read all result files, store results in array, and delete result files
-    for(i = 1; i <= NUM_PROCS; ++i) {
+    for(i = 0; i < NUM_PROCS; ++i) {
         sprintf(filename2, "%s%d", filename, i);
         test_results = fopen(filename2, "r");
         if(test_results) {
-            fscanf(test_results, "%d %d %lu", &(all_res[i-1].proc), &(all_res[i-1].pri), &(all_res[i-1].time));
+            fscanf(test_results, "%d %d %lu %lu", &(all_res[i].proc), &(all_res[i].pri), &(all_res[i].time_s), &(all_res[i].time_ms));
             fclose(test_results);
             remove(filename2);
         } else {
             printf("Can't open result file %d\n", i);
         }
-    }
-
-    //get total priorities and time
-    int pri_tot = 0;
-    int time_tot = 0;
-    for(i = 0; i < NUM_PROCS; ++i) {
-        pri_tot += all_res[i].pri + 21;
-        time_tot += all_res[i].time;
-    }
-
-    //set expected and actual percentages
-    for(i = 0; i < NUM_PROCS; ++i) {
-        all_res[i].per_exp = all_res[i].pri / (double)pri_tot;
-        all_res[i].per_act = all_res[i].time / (double)time_tot;
     }
 
     //sort result array by time
