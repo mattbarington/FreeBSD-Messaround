507190cdefbab5784d3825f7d7a61ee1b20a37b2
Ryan Blelloch <rblelloc@ucsc.edu>
2019-02-24 06:46:32 -0800

    clean up big.cc

--- asgn3/big.cc
+++ asgn3/big.cc
@@ -1,3 +1,15 @@
+/* File: big.cc
+ * File Type: C++ source file
+ * Author(s): Matt Ovenden, Ryan Blelloch
+ *
+ * Description: Benchmark for assignment 3. Compiled with GNU C++.
+ * Builds a vector larger than memory, then accesses it contiguously,
+ * cache ideally, and finally by regularly thrashing. The page fault
+ * messages printed by the system are read and recorded, and the 
+ * number of consecutive vs. nonconsecutive are displayed. A FIFO 
+ * queue should have almost all be consecutive (outliers are due to
+ * special pages (held, etc.)
+*/
 
 #include <iostream>
 #include <vector>
@@ -6,20 +18,11 @@
 #include <sys/time.h>
 #include <stdlib.h>
 
+//Number of values to read from dmesg (true maximum of dmesg ~4500)
 #define SAMPLE_SIZE 10000
 
-void start(long* A, long lenA) {
-  A[0] ++;
-}
-
-void end(long *A, long lenA) {
-  A[lenA - 1] ++;
-}
-
-void middle(long *A, long lenA) {
-  A[lenA/2] ++;
-}
-
+//accesses random pages, but only the first ones
+//no page faults should occur
 void contiguous(long *A, long lenA) {
   int r;
   int h = lenA / 10000;
@@ -33,6 +36,8 @@ void contiguous(long *A, long lenA) {
   }
 }
 
+//accesses the same page every time
+//no page faults should occur
 void cacheIdeal(long *A, long lenA) {
   int r;
   int h = lenA / 10000;
@@ -46,6 +51,8 @@ void cacheIdeal(long *A, long lenA) {
   }
 }
 
+//accesses random pages throughout
+//should cause regular page faults if A is large enough
 void thrashEnds(long *A, long lenA) {
   int r;
   int h = lenA / 10000;
@@ -64,82 +71,106 @@ void printTime(struct timeval tv) {
 }
 
 int main() {
-  unsigned long size = 1139600000;
-  int MBs;
-  int repetitions = 1;
-  double div;
+  
+  //size of memory to allocate
+  unsigned long   size = 1139600000;
+  //user initialized number of megabytes to allocate
+  int             MBs;
+  //number of repetitions to run tests
+  int             repetitions = 1;
+  //iterator
+  int             i; 
+
+  //dmesg results file pointer
+  FILE*           fdmesg;
+  //error from system call
+  int             error;
+  //system call to write free page messages to file
+  char            read_dmesg[] = 
+                    "dmesg | grep \"freeing page:\" > dmesg.dat";
+  //read and ignore strings in dmesg.dat
+  char            buf1[30], buf2[30];
+  //stores the page value identifier
+  unsigned long   page_val;
+
+  //stores the count of consecutive vs. nonconsecutive page values
+  int             cnt1 = 0, cnt2 = 0;
+ 
+  //stores page values from dmesg 
+  unsigned long   sample[SAMPLE_SIZE];
+  //used to store previous page value to compare with
+  unsigned long   prev;
+  //stores the actual sample size
+  int             ss;
+
+  //stores the start, end, and time delta for printing
+  struct timeval start, end, diff;
+  
+  //prompt user for number of MBs to allocate
   std::cout << "how many MBs do you want your vector to span? ";
   std::cin >> MBs;
-  //  std::cout << "How many times would you like to run the thing?: ";
-  //  std::cin >> repetitions;
+  
+  //convert megabytes to bytes
   size = 125000 * MBs;
   printf("size: %lu bytes\n",size*8);
-  //  const unsigned long size = 1139600000;
-  //  const int repetitions = 100;
+
+  //allocate memory
   printf("Setting up\n");
   std::vector<long> V(size);
   srand(0);
-  struct timeval start, end, diff;
   printf("Set up finished\n");
 
+  //test memory with similar pages (no page faults)
   gettimeofday(&start, 0);
-  for (int i = 0; i < repetitions; i++)
+  for (i = 0; i < repetitions; i++)
     cacheIdeal(V.data(), V.size());
   gettimeofday(&end, 0);
   timersub(&end, &start, &diff);
+  printf("Similar pages execution time: ");
   printTime(diff);
 
+  //test memory at cache ideal positions (no page faults)
   gettimeofday(&start, 0);
-  for (int i = 0; i < repetitions; i++)
+  for (i = 0; i < repetitions; i++)
     contiguous(V.data(), V.size());
   gettimeofday(&end, 0);
   timersub(&end, &start, &diff);
+  printf("Cache ideal pages execution time: ");
   printTime(diff);
 
-
+  //test memory at random pages across memory (lots of page faults)
   gettimeofday(&start, 0);
-  for (int i = 0; i < repetitions; i++)
+  for (i = 0; i < repetitions; i++)
     thrashEnds(V.data(), V.size());
   gettimeofday(&end, 0);
   timersub(&end, &start, &diff);
+  printf("Random pages execution time: ");
   printTime(diff);
 
-  FILE* fdmesg;
-  int error, iter;
-  char read_dmesg[] = "dmesg | grep \"freeing pages\" > dmesg.dat";
-  char buf1[30];
-  char buf2[30];
-  unsigned long page_val;
 
-  int cnt1 = 0, cnt2 = 0;
-  
   //write results to file
   error = system(read_dmesg);
-  unsigned long sample[SAMPLE_SIZE];
-
   //attempt to open file
   fdmesg = fopen("dmesg.dat", "r");
-  iter = 0;
-  int ss;
-  unsigned long prev;
+  i = 0;
   if(fdmesg) {
-    while((iter < SAMPLE_SIZE) && fscanf(fdmesg, "%s %s %lu", buf1, buf2, &page_val) != EOF) {
-      sample[iter] = page_val;
-      ++iter;
+    while((i < SAMPLE_SIZE) && 
+          fscanf(fdmesg, "%s %s %lu", buf1, buf2, &page_val) != EOF) {
+      sample[i] = page_val;
+      ++i;
     }
   } else {
     printf("ERROR: dmesg.dat could not be opened.\n");
   }
-  ss = iter;
-
+  ss = i;
   prev = sample[0];
-  for(iter = 1; iter < ss; ++iter) {
-    if(prev == (sample[iter] - 1)) {
+  for(i = 1; i < ss; ++i) {
+    if(prev == (sample[i] - 1)) {
       cnt1++;
     } else {
       cnt2++;
     }
-    prev = sample[iter];
+    prev = sample[i];
   }
 
   printf("consecutive: %d, other: %d\n", cnt1, cnt2);
