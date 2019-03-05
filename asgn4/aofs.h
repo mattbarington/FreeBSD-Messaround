7bf958e128b7a168a08d71304e9112f30635b02e
Ryan Blelloch <rblelloc@ucsc.edu>
2019-03-05 15:14:43 -0800

    stucturing the aofs

--- asgn4/aofs.h
+++ asgn4/aofs.h
@@ -1,14 +1,16 @@
 #ifndef AOFS_H
 #define AOFS_H
 
+#include <stdbool.h>
+
+#define byte unsigned char
 
 //Magic number
 #define MAGICNUM 0xfa19283e
 //Block size
-#define BLOCK_SIZE 4096
+#define DB_BLOCK_SIZE 4096
 //Number of blocks
-#define BLOCK_NUM 512
-	 
+#define BLOCK_NUM 4096
 
 typedef Superblock {
   uint32_t magicnum;
@@ -17,7 +19,30 @@ typedef Superblock {
   uint8_t bitmap[BLOCK_NUM / sizeof(uint8_t)];
 } Superblock;
 
+/*
+typedef Datablockmeta {
+  char filename[256];
+  Datablock* next;
+  bool head;
+} Datablockmeta;
 
+//Datablock metadata size
+#define DB_BLOCK_META sizeof(Datablockmeta)
+#define DB_BLOCK_DATA (DB_BLOCK_SIZE - DB_BLOCK_META)
+
+typedef Datablock {
+  byte data[DATA_SIZE]
+} Datablock;
+
+typedef Datablocks {
+  Datablock[BLOCK_NUM];
+} Datablocks;
+
+typedef AOFS {
+  Superblock sb;
+  Datablocks db;
+} AOFS;
+*/
 #endif
 
 
