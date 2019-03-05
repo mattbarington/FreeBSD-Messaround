7ade1cd52ac2df5a09c8117950aa8e7a834204d3
Ryan Blelloch <rblelloc@ucsc.edu>
2019-03-05 15:23:11 -0800

    my changes

--- asgn4/aofs.h
+++ asgn4/aofs.h
@@ -1,7 +1,10 @@
 #ifndef AOFS_H
 #define AOFS_H
 
+#include <stdlib.h>
+#include <stdio.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 #define byte unsigned char
 
@@ -12,37 +15,35 @@
 //Number of blocks
 #define BLOCK_NUM 4096
 
-typedef Superblock {
+typedef struct Superblock {
   uint32_t magicnum;
   uint32_t totalblocks;
   uint32_t blocksize;
   uint8_t bitmap[BLOCK_NUM / sizeof(uint8_t)];
 } Superblock;
 
-/*
-typedef Datablockmeta {
+struct Block;
+
+typedef struct BlockMeta {
   char filename[256];
-  Datablock* next;
+  struct Block* next;
   bool head;
-} Datablockmeta;
+} BlockMeta;
 
 //Datablock metadata size
-#define DB_BLOCK_META sizeof(Datablockmeta)
+#define DB_BLOCK_META sizeof(BlockMeta)
 #define DB_BLOCK_DATA (DB_BLOCK_SIZE - DB_BLOCK_META)
 
-typedef Datablock {
-  byte data[DATA_SIZE]
-} Datablock;
+typedef struct Block {
+  BlockMeta dbm;
+  byte data[DB_BLOCK_DATA];
+} Block;
 
-typedef Datablocks {
-  Datablock[BLOCK_NUM];
-} Datablocks;
-
-typedef AOFS {
+typedef struct AOFS {
   Superblock sb;
-  Datablocks db;
+  Block db[BLOCK_NUM];
 } AOFS;
-*/
+
 #endif
 
 
