ed8157d8b10e5d639b79538f0ff4ce51bcf238c0
Matt Ovenden <movenden@ucsc.edu>
2019-03-05 08:49:40 -0800

    im gonna push like a pregnant lady

--- asgn4/aofs.h
+++ asgn4/aofs.h
@@ -2,5 +2,20 @@
 #define AOFS_H
 
 #define MAGICUM 69
+#define
+
+typedef Superblock {
+  uint32_t magicnum;
+  uint32_t totalblocks;
+  uint32_t bitmap[];
+  
+  
+
+} Superblock;
+
+
 
 #endif
+
+
+
