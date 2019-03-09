473fda733154c56041b4d202b0ee16fbd8196c5d
Ryan Blelloch <rblelloc@ucsc.edu>
2019-03-09 03:49:03 -0800

    starting to write the benchmark

--- asgn4/benchmark.c
+++ asgn4/benchmark.c
@@ -1,36 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+//#include <time.h>
 #include <string.h>
+#include <sys/time.h>
 
 //#define NUM_ARGS 2
+
+//https://stackoverflow.com/questions/307596/time-difference-in-c
+int diff_ms(struct timeval t1, struct timeval t2)
+{
+    return (((t1.tv_sec - t2.tv_sec) * 1000000) + 
+            (t1.tv_usec - t2.tv_usec))/1000;
+}
 
 int main(int argc, char* argv[]) {
 
   double aofs_write_t;
-  time_t aofs_write_s, aofs_write_f;
+  struct timeval aofs_write_s, aofs_write_f;
   double fbsd_write_t;
-  time_t fbsd_write_s, fbsd_write_f;
+  struct timeval fbsd_write_s, fbsd_write_f;
   double aofs_create_t;
-  time_t aofs_create_s, aofs_create_f;
+  struct timeval aofs_create_s, aofs_create_f;
   double fbsd_create_t;
-  time_t fbsd_create_s, fbsd_create_f;
+  struct timeval fbsd_create_s, fbsd_create_f;
   double aofs_read_t;
-  time_t aofs_read_s, aofs_read_f;
+  struct timeval aofs_read_s, aofs_read_f;
   double fbsd_read_t;
-  time_t fbsd_read_s, fbsd_read_f;
+  struct timeval fbsd_read_s, fbsd_read_f;
 
   char run_aofs[] = "./init_fs ; ./run_fs aofs_test";
 
   char fbsd_folder[] = "fbsd_test";
   char aofs_folder[] = "aofs_test";
-  char goto_fbsd_folder[] = "cd fbsd_test";
-  char goto_aofs_folder[] = "cd ../aofs_test";
   char make_fbsd_folder[] = "mkdir fbsd_test";
   char make_aofs_folder[] = "mkdir aofs_test";
+  char remove_fbsd_folder[] = "rm -rf fbsd_test";
+  char remove_aofs_folder[] = "rm -rf aofs_test";
 
-  char touch_100_files[] = "touch test_file";
-  char write_file[] = "echo boi > test_file1";
+  char fbsd_touch_100_files[] = "touch fbsd_test/test_file";
+  char aofs_touch_100_files[] = "touch aofs_test/test_file";
+  char fbsd_write_file[] = "echo boi > fbsd_test/test_file1";
+  char aofs_write_file[] = "echo boi > aofs_test/test_file1";
   char read_file[] = "cat test_file1";
 
   //check for correct number of arguments
@@ -44,20 +54,22 @@ int main(int argc, char* argv[]) {
   system(make_aofs_folder);
 
   //run tests on FreeBSD file system
-  system(goto_fbsd_folder);
   int i;
   char touch_command[200];
   char numval[12];
-  fbsd_create_s = time(NULL);
+  gettimeofday(&fbsd_create_s, NULL);
   for(i = 0; i < 100; ++i) {
-    itoa(i, numval, 10);
-    strcpy(touch_command, touch_100_files);
+    sprintf(numval, "%d", i);
+    strcpy(touch_command, fbsd_touch_100_files);
     strcat(touch_command, numval);
     system(touch_command);
   }
-  fbsd_create_f = time(NULL);
-  fbsd_create_t = difftime(fbsd_create_f, fbsd_create_s);
+  gettimeofday(&fbsd_create_f, NULL);
+  printf("%d\n", diff_ms(fbsd_create_f, fbsd_create_s));
   printf("FreeBSD create 100: %lf\n", fbsd_create_t);
   
+  system(remove_fbsd_folder);
+  system(remove_aofs_folder);
+
   return 0;
 }
